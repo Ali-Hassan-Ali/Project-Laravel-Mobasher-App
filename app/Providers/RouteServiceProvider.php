76d81cc6d1ccd99fc5e754b8e3d41fb4365fae07
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-04-07 07:39:47 +0200

    add

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -54,7 +54,6 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/admin.php'));
 
             Route::prefix('api')
-                ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
