4469b37667410c91ddc26f1ae7a2366fc72691c8
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-04-12 05:35:28 +0200

    add

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Apartment;
 use App\Http\Requests\RegisterRequest;
 
 class UserAuthController extends Controller
