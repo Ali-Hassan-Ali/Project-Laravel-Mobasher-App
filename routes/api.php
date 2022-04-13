b08c162d665742298bd1c1a2c398fd21dcd01be5
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-04-13 04:29:42 +0200

    add

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::post('ping/post', function(Request $request) {
     return "work post 🤡";
 });
 
-Route::get('/search',[SearchController::class,'search']);
+Route::post('/search',[SearchController::class,'search']);
 Route::post('/advanced_search',[SearchController::class,'advanced_search']);
 
 Route::post('/order/store',[OrderController::class,'store']);
