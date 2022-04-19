0498437259fb434fefd606723dd025c4ede789af
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-04-19 23:12:11 +0200

    add

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -29,17 +29,27 @@ class ApartmentController extends Controller
 
         }//end of if
 
-        $apartment = Apartment::create([
-                                        'city'  => $request->input('city'),
-                                        'state' => $request->input('state'),
-                                        'type'  => $request->input('type'),
-                                        'floor' => $request->input('floor'),
-                                        'price' => $request->input('price'),
-                                        'rooms' => $request->input('rooms'),
-                                        'dimensions'  => $request->input('dimensions'),
-                                        'description' => $request->input('description'),
-                                        'user_id'     => $request->input('user_id'),
-                                       ]);
+        $apartment = Apartment::create($request->only('city',
+                                                      'state',
+                                                      'type',
+                                                      'floor',
+                                                      'price',
+                                                      'rooms',
+                                                      'dimensions',
+                                                      'description',
+                                                      'user_id'));
+
+        // $apartment = Apartment::create([$request->input('city')
+        //                                 'city'  => ,
+        //                                 'state' => $request->input('state'),
+        //                                 'type'  => $request->input('type'),
+        //                                 'floor' => $request->input('floor'),
+        //                                 'price' => $request->input('price'),
+        //                                 'rooms' => $request->input('rooms'),
+        //                                 'dimensions'  => $request->input('dimensions'),
+        //                                 'description' => $request->input('description'),
+        //                                 'user_id'     => $request->input('user_id'),
+        //                                ]);
         
         $apartment = Apartment::findOrFail($apartment->id);
 
