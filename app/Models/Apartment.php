6d80d04d3fe2bfd48d3f1e67a7b2e7286b2bb6be
https://github.com/Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-08-16 11:11:54 +0200

    add

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Rent;
-use App\Models\media;
 
 class Apartment extends Model
 {
